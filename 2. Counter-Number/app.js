8188f72c71529cb483308f5e5444ebaeec4380ad
Saddam Hafiz Alif <mr.alif.dev@gmail.com>
2023-09-24 17:25:25 +0600

    some change count project

--- 2. Counter-Number/app.js
+++ 2. Counter-Number/app.js
@@ -1,17 +1,15 @@
 const value = document.getElementById("value");
 const btns = document.querySelectorAll(".btn");
 
-console.log(btns);
 let count = 0;
 
 btns.forEach(function (btn) {
     btn.addEventListener("click", function(e) {
-        let styles = e.currentTarget.classList;
-
-        console.log(styles);
-        if(styles.contains("decrease")) {
+        const styles = e.currentTarget.classList;
+        
+        if(styles.contains('decrease')) {
             count--;
-        }else if(styles.contains("increase")) {
+        }else if(styles.contains('increase')) {
             count++;
         }else{
             count = 0;
@@ -20,12 +18,10 @@ btns.forEach(function (btn) {
         value.textContent = count;
 
         if(count > 0) {
-            value.style.color = "green"
-        }
-        if(count < 0) {
-            value.style.color = "red"
-        }
-        if(count === 0) {
+            value.style.color = "green";
+        }else if(count < 0) {
+            value.style.color = "red";
+        }else{
             value.style.color = "black";
         }
     })
